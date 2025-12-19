8a7cac4e4a8b440c9aea5b9c787fc3b14fc1e0f2
MinhazulAlamin <minhazulalamin007@gmail.com>
2025-12-19 17:53:18 +0600

    users code added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const stripe = require('stripe');
 const port = process.env.PORT || 3000;
 
 //middlewire
@@ -25,16 +25,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const db = client.db("ScholarshipStream");
+
+ 
+    //Users Related APIs
+
+        const usersCollection = db.collection("Users");
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      const existingUser = await usersCollection.findOne({
+        email: newUser.email,
+      });
+
+      if (existingUser) {
+        return res.send({
+          message: "User already exists",
+        });
+      }
+
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
